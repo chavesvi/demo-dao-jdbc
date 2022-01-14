ab1fe3fa206ae680e1952b6210fe16d9c76573ff
Vitória Chaves <vitoriarsc04@gmail.com>
2022-01-14 10:39:33 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import jdbc.connector.ConexaoUtil;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
-	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+	public static SellerDao createSellerDao(String url, String user, String password) {
+		return new SellerDaoJDBC(ConexaoUtil.getConnection(url, user, password));
 	}
 
 }
